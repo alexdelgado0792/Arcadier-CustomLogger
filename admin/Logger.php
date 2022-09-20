3c061755b3e46502a1eaae4fd8d90f4c87277e33
Alex Delgado <arekkusu0792@gmail.com>
2022-09-20 10:58:24 -0500

    init endpoint for Log

--- admin/Logger.php
+++ admin/Logger.php
@@ -2,3 +2,11 @@
     Endpoint to sent logs via HTTP Request using authentication
 
  -->
+<?php 
+
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+// include('ADevTools/CustomLog/')
+
+?>
+
