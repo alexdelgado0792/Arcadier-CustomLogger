91c832642148df4dfe5918594d057cfae108c583
Alex Delgado <arekkusu0792@gmail.com>
2022-09-20 11:52:09 -0500

    add some validations and get configuration from ct

--- admin/ADevTools/CustomLog/CustomLogger.php
+++ admin/ADevTools/CustomLog/CustomLogger.php
@@ -17,25 +17,38 @@ class CustomLogger
     {
         $this->arcadier = new ArcadierApi($clientId, $clientSecret);
 
-        $configParams = $this->arcadier->SearchCt('Configuration', 'request body');
+        $configParams = $this->arcadier->GetAllCtContent('Configuration');
 
-        $this->sendEmails = $configParams["SendEmail"];
-        $this->emailsToBeSend = $configParams["Emails"];
-        $this->emailBody = $configParams["EmailBody"];
-        $this->emailfrom = $configParams["EmailFrom"];
-        $this->emailSubject = $configParams["EmailSubject"];
+        if ($configParams["TotalRecords"] == 0) {
+            throw new Exception("At least one configuration must exists. Current is " . $configParams["TotalRecords"]);
+        }
+
+        if ($configParams["TotalRecords"] > 1) {
+            throw new Exception("Just one configuration must exists. Current is " . $configParams["TotalRecords"]);
+        }
+
+        if ($configParams["TotalRecords"] == 1) {
+            $this->sendEmails = $configParams["Records"]["SendEmail"];
+            $this->emailsToBeSend = $configParams["Records"]["Emails"];
+            $this->emailBody = $configParams["Records"]["EmailBody"];
+            $this->emailfrom = $configParams["Records"]["EmailFrom"];
+            $this->emailSubject = $configParams["Records"]["EmailSubject"];
+        }
+        else {
+            throw new Exception("Some error occur but is an unknown error.");
+        }
+
     }
 
     function Log()
     {
-        //here send email 
+    //here send email 
     }
 
     function SendEmails()
     {
         //Execute only if flag is activated
-        if(boolval($this->sendEmails))
-        {
+        if (boolval($this->sendEmails)) {
             foreach ($this->GetEmails() as $email) {
                 $this->arcadier->SendEmail($this->emailfrom, $email, $this->emailBody, $this->emailSubject);
             }
